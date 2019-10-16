afe0a8610fe9c4d30af454f11a7ae4646b5f1c5c
Mike <mike.huang.mikank@gmail.com>
2019-10-16 21:35:49 +0800

    Create order process on home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,7 +7,15 @@
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 export default {
   name: 'App'
 }
 </script>
+
+<style lang="scss">
+body {
+  background-color: #F4F7F8;
+  color: color(secondary);
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,17 +2,20 @@
   <header>
     <Navbar />
     <HomeBanner />
+    <OrderProcess />
   </header>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 import HomeBanner from '../components/HomeBanner'
+import OrderProcess from '../components/OrderProcess'
 export default {
   name: 'Home',
   components: {
     Navbar,
-    HomeBanner
+    HomeBanner,
+    OrderProcess
   }
 }
 </script>
