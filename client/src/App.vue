9fd0469689999e84a7b18e479295263bd2012a41
Mike <mike.huang.mikank@gmail.com>
2019-10-16 17:01:54 +0800

    Create navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,3 +3,11 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+export default {
+  name: 'App'
+}
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1>Landing pages</h1>
+  <header>
+    <Navbar />
+  </header>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
 export default {
-  name: "Home",
-  components: {}
-};
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
 </script>
