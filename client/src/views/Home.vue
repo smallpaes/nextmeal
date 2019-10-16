5cffec11f7b6ff55781c1f8188d0b3a2d30ff376
Mike <mike.huang.mikank@gmail.com>
2019-10-16 14:58:05 +0800

    Config basic view, route, component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Landing pages</h1>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
