2c6869237e24e8f0edc49e595cf8df599e113114
Mike <mike.huang.mikank@gmail.com>
2019-10-17 17:37:23 +0800

    Init restaurants view and route

---
+++ client/src/views/Restaurants.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <header>
+      <Navbar />
+    </header>
+    <Footer />
+  </section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
