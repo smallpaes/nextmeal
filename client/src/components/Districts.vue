374c14a9cf0fa408209e29f73d706782230c4ffd
Mike <mike.huang.mikank@gmail.com>
2019-10-17 13:18:16 +0800

    Create distrcit section component

---
+++ client/src/components/Districts.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row">
+    <a
+      v-for="district in districts"
+      :key="district.eng_name"
+      class="img-wrapper col-12 col-md-6 col-lg-3 mb-3 px-2"
+      href="#"
+    >
+      <div class="img-container">
+        <img
+          :src="district.image"
+          alt="Restaurant image"
+          class="img-fluid shadow-sm rounded-sm"
+        >
+        <div class="img-overlay" />
+        <h3 class="img-content">
+          {{ district.chinese_name }}
+        </h3>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    districts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.img {
+    @include positionCenter;
+    @include imgOverlay;
+
+    &-container {
+        transition: transform .2s linear;
+
+        &:hover {
+            transform: translateY(-2%);
+        }
+    }
+
+    &-fluid {
+        height: 180px;
+        width: 100%;
+        object-fit: cover;
+    }
+
+    &-content {
+        color: color(quaternary);
+    }
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -18,6 +18,19 @@
         <RestaurantCarousel :popular-restaurants="popular_restaurants" />
       </div>
     </section>
+    <section class="districts">
+      <div class="container pt-3 pb-5">
+        <div class="popular-heading">
+          <h1 class="popular-heading-title">
+            熱門地區
+          </h1>
+          <p class="popular-heading-description">
+            探索各地區熱門美食
+          </p>
+        </div>
+        <Districts :districts="districts" />
+      </div>
+    </section>
   </section>
 </template>
 
@@ -26,6 +39,7 @@ import Navbar from '../components/Navbar'
 import HomeBanner from '../components/HomeBanner'
 import OrderProcess from '../components/OrderProcess'
 import RestaurantCarousel from '../components/RestaurantCarousel'
+import Districts from '../components/Districts'
 
 const dummyRestaurantAndDistrict = {
   popular_restaurants: [
@@ -66,7 +80,36 @@ const dummyRestaurantAndDistrict = {
       category: '中東料理'
     }
   ],
-  districts: ['大安區', '中山區', '松山區', '信義區']
+  districts: [
+    {
+      'chinese_name': '大安區',
+      'eng_name': "Da'an",
+      'image': 'https://cdn.pixabay.com/photo/2013/11/13/21/14/san-francisco-210230_1280.jpg',
+      'lng': '121.5434446',
+      'lat': '25.02677012'
+    },
+    {
+      'chinese_name': '信義區',
+      'eng_name': 'Xinyi',
+      'image': 'https://cdn.pixabay.com/photo/2019/09/23/14/34/nyc-4498752_1280.jpg',
+      'lng': '121.5716697',
+      'lat': '25.03062083'
+    },
+    {
+      'chinese_name': '中山區',
+      'eng_name': 'Zhongshan',
+      'image': 'https://cdn.pixabay.com/photo/2018/04/25/09/26/eiffel-tower-3349075_1280.jpg',
+      'lng': '121.7308913',
+      'lat': '25.14986365'
+    },
+    {
+      'chinese_name': '松山區',
+      'eng_name': 'Songshan',
+      'image': 'https://cdn.pixabay.com/photo/2016/10/31/04/19/lan-yang-museum-1784871_1280.jpg',
+      'lng': '121.5575876',
+      'lat': '25.05999101'
+    }
+  ]
 }
 
 export default {
@@ -75,7 +118,8 @@ export default {
     Navbar,
     HomeBanner,
     OrderProcess,
-    RestaurantCarousel
+    RestaurantCarousel,
+    Districts
   },
   data () {
     return {
