7184d6d9559fd775b3973f63d966e48bdc6243d8
Tao <ctaohe@gmail.com>
2019-10-18 10:41:41 +0800

    Add owner get restaurant information

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,14 +1,21 @@
 const db = require('../models')
+const Restaurant = db.Restaurant
 
 let ownerController = {
   getRestaurant: async (req, res) => {
     try {
-      res.status(200).json({status: 'success', message: 'Successfully get the restaurant information.'})
+      let restaurant = await Restaurant.findAll({
+        where: {UserId: req.user.id}
+      })
+      if (restaurant.length === 0) {
+        return res.status(200).json({status: 'success',  message: 'You have not restaurant yet.'})
+      }
+      res.status(200).json({status: 'success', restaurant, message: 'Successfully get the restaurant information.'})
     } catch (error) {
       res.status(500).json({ status: 'error', message: error })
     }
-  }
-
+  },
+  
 }
 
 module.exports = ownerController
