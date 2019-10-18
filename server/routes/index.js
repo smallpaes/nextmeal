76b8c5f039642a074ac943fc815e0e7ca7f5c31c
Danny <windate3411@gmail.com>
2019-10-18 14:33:20 +0800

    finished owner.spec.js (besides orders by time period)

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ const { ensureAuthenticated, isAuthAdmin, getUser } = require('../config/auth')
 const db = require('../models')
 const User = db.User
 const Restaurant = db.Restaurant
+const Meal = db.Meal
+const Order = db.Order
 module.exports = (app) => {
   app.get('/api', (req, res) => {
     console.log(req.query);
@@ -45,13 +47,42 @@ module.exports = (app) => {
     }
   })
 
-  app.get('/api/admin/restaurants', ensureAuthenticated, getUser, isAuthAdmin, async (req, res) => {
-    // const whereClause = {}
-    // if (req.query.payment_status) {
-    //   wherequery['payment_status'] = req.query.payment_status
-    // }
+  app.get('/api/admin/restaurants', async (req, res) => {
 
-    const restaurants = await Restaurant.findAll()
+    let restaurants = await Restaurant.findAll({
+      include: [{
+        model: Meal,
+        include: [{
+          model: Order, as: 'orders', attributes: ['id', 'order_status',]
+        }],
+        attributes: ['id']
+      }]
+    })
+    restaurants = restaurants.map(restaurant => ({
+      ...restaurant.dataValues,
+      orderCount: restaurant.dataValues.Meals[0].dataValues.orders.length
+    }))
     return res.json({ restaurants })
+  })
+
+  app.post('/api/owner', ensureAuthenticated, getUser, async (req, res) => {
+    const restaurant = await Restaurant.create({ UserId: req.user.id, name: req.body.name })
+    return res.status(200).json({ status: 'success', message: 'successfuly add a new restaurant' })
+  })
+
+  app.get('/api/owner', ensureAuthenticated, getUser, async (req, res) => {
+    const restaurant = await Restaurant.findOne({ where: { UserId: req.user.id } })
+    return res.status(200).json({ restaurant })
+  })
+
+  app.put('/api/owner', ensureAuthenticated, getUser, async (req, res) => {
+    const restaurant = await Restaurant.findOne({ where: { UserId: req.user.id } })
+    await restaurant.update({ name: req.body.name })
+    return res.status(200).json({ status: 'success', message: 'successfuly add a new restaurant' })
+  })
+
+  app.get('/api/owner/dishes', ensureAuthenticated, getUser, async (req, res) => {
+    const meals = await Meal.findAll({ where: { RestaurantId: 1 } })
+    return res.status(200).json({ meals })
   })
 }
