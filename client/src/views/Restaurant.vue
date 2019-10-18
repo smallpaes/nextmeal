5ee61f807a61ef9d9913ec3fa9f552acbb3d53c0
Mike <mike.huang.mikank@gmail.com>
2019-10-18 16:14:57 +0800

    Create breadcrumb and restaurant info component, and add to restaurant page

--- client/src/views/Restaurant.vue
+++ client/src/views/Restaurant.vue
@@ -6,6 +6,12 @@
         :background-photo="restaurant.image"
       />
     </header>
+    <div class="info container mt-2">
+      <Breadcrumb
+        :restaurant="restaurant"
+      />
+      <RestaurantInfo :restaurant="restaurant" />
+    </div>
     <Footer />
   </section>
 </template>
@@ -14,13 +20,20 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ImageBanner from '../components/Banner/ImageBanner'
+import Breadcrumb from '../components/Breadcrumb'
+import RestaurantInfo from '../components/RestaurantInfo'
 
 const dummyRestaurantAndDistrict = {
-  restaurant: { id: 1,
+  restaurant: {
+    id: 1,
     image: 'https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706_1280.jpg',
     name: '餐廳一號',
     rating: 3.4,
-    category: '美式料理'
+    category: {
+      name: '美式料理'
+    },
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in',
+    location: '信義區'
   },
   comments: {
     count: 16,
@@ -72,22 +85,22 @@ const dummyRestaurantAndDistrict = {
       }
     ]
   },
-  district: [
-    {
-      'chinese_name': '大安區',
-      'eng_name': "Da'an",
-      'image': 'https://cdn.pixabay.com/photo/2013/11/13/21/14/san-francisco-210230_1280.jpg',
-      'lng': '121.5434446',
-      'lat': '25.02677012'
-    }
-  ]
+  district: {
+    'chinese_name': '大安區',
+    'eng_name': "Da'an",
+    'image': 'https://cdn.pixabay.com/photo/2013/11/13/21/14/san-francisco-210230_1280.jpg',
+    'lng': '121.5434446',
+    'lat': '25.02677012'
+  }
 }
 
 export default {
   components: {
     Navbar,
     Footer,
-    ImageBanner
+    ImageBanner,
+    Breadcrumb,
+    RestaurantInfo
   },
   data () {
     return {
