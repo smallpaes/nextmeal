519437fcaa86f2f78c784cb91343caa212132312
Tao <ctaohe@gmail.com>
2019-10-27 16:27:36 +0800

    fix bug

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -11,9 +11,6 @@ router.get('/subscribe',ensureAuthenticated, userController.getSubscription)
 router.post('/subscribe',ensureAuthenticated, userController.postSubscription)
 router.post('/subscribe/spgateway/callback', userController.spgatewayCallback)
 
-router.get('/:user_id', ensureAuthenticated, getUser, userController.getProfile)
-router.put('/:user_id/edit', ensureAuthenticated, getUser, upload.single('avatar'), validUserProfile, userController.putProfile)
-
 //user signup/signin related
 router.get('/signup', userController.getCategories)
 router.post('/emailcheck', validator.creatUser, userController.emailCheck)
