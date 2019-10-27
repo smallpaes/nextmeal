9c9df1f4183d2756808969b22776be5fb64716cc
Tao <ctaohe@gmail.com>
2019-10-27 11:49:09 +0800

    Change valid method and add new middleware and change create sub method

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,6 +1,8 @@
 const { check, validationResult } = require('express-validator')
 const moment = require('moment')
 const crypto = require("crypto"); // 加密
+const db = require('../models')
+const Subscription = db.Subscription
 
 const URL = 'https://f798ad18.ngrok.io'; //本地 domain 不接受，使用 ngrok 工具做臨時網址，取得的網址放這
 const MerchantID = 'MS38035958'; // 商店代號
@@ -11,46 +13,78 @@ const ReturnURL = URL + "/api/user/subscribe/spgateway/callback?from=ReturnURL";
 const NotifyURL = URL + "/api/user/subscribe/spgateway/callback?from=NotifyURL"; //支付通知網址
 const ClientBackURL = URL + "/api"; //支付取消返回網址
 
+// 將資料轉成字串
+const genDataChain = (TradeInfo) => {
+  let results = [];
+  for (let kv of Object.entries(TradeInfo)) {
+    results.push(`${kv[0]}=${kv[1]}`);
+  }
+  return results.join("&");
+}
+
+// 轉成字串後 AES 加密
+const create_mpg_aes_encrypt = (TradeInfo) => {
+  let encrypt = crypto.createCipheriv("aes256", HashKey, HashIV);
+  let enc = encrypt.update(genDataChain(TradeInfo), "utf8", "hex");
+  return enc + encrypt.final("hex");
+}
+
+// 加密後再用 sha 做雜湊
+const create_mpg_sha_encrypt = (TradeInfo) => {
+  let sha = crypto.createHash("sha256");
+  let plainText = `HashKey=${HashKey}&${TradeInfo}&HashIV=${HashIV}`;
+
+  return sha
+    .update(plainText)
+    .digest("hex")
+    .toUpperCase();
+}
+
 let middleware = {
+  creatUser: [
+    check('email')
+      .not().isEmpty().withMessage('Email should be not empty')
+      .isEmail().withMessage('This is not Email address'),
+  ],
   validRestaurantForm: [
     check('name')
-      .not().isEmpty().withMessage('name should be not empty'),
+      .not().isEmpty().withMessage('Name should be not empty'),
     check('description')
-      .not().isEmpty().withMessage('description should be not empty'),
+      .not().isEmpty().withMessage('Description should be not empty'),
     check('tel')
-      .not().isEmpty().withMessage('phone should be not empty'),
+      .not().isEmpty().withMessage('Phone should be not empty'),
     check('address')
-      .not().isEmpty().withMessage('address should be not empty')
+      .not().isEmpty().withMessage('Address should be not empty')
   ],
   validMenuForm: [
     check('quantity')
-      .not().isEmpty().withMessage('quantity should be not empty'),
+      .not().isEmpty().withMessage('Quantity should be not empty'),
   ],
   validDishForm: [
     check('name')
-      .not().isEmpty().withMessage('name should be not empty'),
+      .not().isEmpty().withMessage('Name should be not empty'),
     check('description')
-      .not().isEmpty().withMessage('description should be not empty'),
+      .not().isEmpty().withMessage('Description should be not empty'),
   ],
   validUserProfile: [
     check('name')
-      .not().isEmpty().withMessage('name should be not empty'),
+      .not().isEmpty().withMessage('Name should be not empty'),
     check('email')
-      .not().isEmpty().withMessage('email should be not empty'),
-    check('location')
-      .not().isEmpty().withMessage('location should be not empty'),
+      .not().isEmpty().withMessage('Email should be not empty')
+      .isEmail().withMessage('This is not Email address'),
     check('address')
-      .not().isEmpty().withMessage('address should be not empty'),
+      .not().isEmpty().withMessage('Address should be not empty'),
     check('dob')
-      .not().isEmpty().withMessage('birthday should be not empty'),
+      .not().isEmpty().withMessage('Birthday should be not empty'),
     check('prefer')
-      .not().isEmpty().withMessage('prefer should be not empty'),
+      .not().isEmpty().withMessage('Prefer should be not empty'),
   ],
   validOrderForm: [
     check('require_date')
-      .not().isEmpty().withMessage('require_date should be not empty'),
+      .not().isEmpty().withMessage('Require_date should be not empty'),
     check('quantity')
-      .not().isEmpty().withMessage('quantity should be not empty'),
+      .not().isEmpty().withMessage('Quantity should be not empty')
+      .isInt().withMessage('Quantity should be a integer'),
   ],
   validMessage: (req, res) => {
     const errors = validationResult(req)
@@ -68,36 +102,12 @@ let middleware = {
     }
     return array
   },
-  // 將資料轉成字串
-  genDataChain: (TradeInfo) => {
-    let results = [];
-    for (let kv of Object.entries(TradeInfo)) {
-      results.push(`${kv[0]}=${kv[1]}`);
-    }
-    return results.join("&");
-  },
-  // 轉成字串後 AES 加密
-  create_mpg_aes_encrypt: (TradeInfo) => {
-    let encrypt = crypto.createCipheriv("aes256", HashKey, HashIV);
-    let enc = encrypt.update(genDataChain(TradeInfo), "utf8", "hex");
-    return enc + encrypt.final("hex");
-  },
-  // 加密後再用 sha 做雜湊
-  create_mpg_sha_encrypt: (TradeInfo) => {
-    let sha = crypto.createHash("sha256");
-    let plainText = `HashKey=${HashKey}&${TradeInfo}&HashIV=${HashIV}`;
 
-    return sha
-      .update(plainText)
-      .digest("hex")
-      .toUpperCase();
-  },
-  // 將加密後的資料打包成 TradeInfo return
   getTradeInfo: (Amt, Desc, email, sn) => {
     console.log("===== getTradeInfo =====");
     console.log(Amt, Desc, email, sn);
     console.log("==========");
-
+  
     data = {
       MerchantID: MerchantID, // 商店代號
       RespondType: "JSON", // 回傳格式
@@ -113,17 +123,17 @@ let middleware = {
       NotifyURL: NotifyURL, // 支付通知網址/每期授權結果通知
       ClientBackURL: ClientBackURL // 支付取消返回商店網址
     };
-
+  
     console.log("===== getTradeInfo: data =====");
     console.log(data);
-
+  
     mpg_aes_encrypt = create_mpg_aes_encrypt(data);
     mpg_sha_encrypt = create_mpg_sha_encrypt(mpg_aes_encrypt);
-
+  
     console.log("===== getTradeInfo: mpg_aes_encrypt, mpg_sha_encrypt =====");
     console.log(mpg_aes_encrypt);
     console.log(mpg_sha_encrypt);
-
+  
     tradeInfo = {
       MerchantID: MerchantID, // 商店代號
       TradeInfo: mpg_aes_encrypt, // 加密後參數
@@ -132,10 +142,10 @@ let middleware = {
       PayGateWay: PayGateWay,
       MerchantOrderNo: data.MerchantOrderNo
     };
-
+  
     console.log("===== getTradeInfo: tradeInfo =====");
     console.log(tradeInfo);
-
+  
     return tradeInfo;
   },
   // 交易完成後回傳資料使用的反向解密
@@ -146,7 +156,23 @@ let middleware = {
     let plainText = text + decrypt.final("utf8");
     let result = plainText.replace(/[\x00-\x20]+/g, "");
     return result;
+  },
+  createSubscription: async (req, res, tradeInfo) => {
+    try {
+      let subscription = await Subscription.create({
+        UserId: req.user.id,
+        sub_name: req.body.sub_name,
+        sub_price: req.body.sub_price,
+        payment_status: 0,
+        sub_description: req.body.sub_description,
+        sub_balance: req.body.sub_balance,
+        sn: tradeInfo.MerchantOrderNo,
+      })
+      return subscription
+    } catch (error) {
+      res.json({ status: 'error', message: error })
+    }
   }
 }
 
-module.exports = middleware
+module.exports = { tradeInfo } = middleware
