af8b1853760393d48fc35d2b9bca0290c516c5ff
Mike <mike.huang.mikank@gmail.com>
2019-10-29 12:46:25 +0800

    Update config

--- server/app.js
+++ server/app.js
@@ -14,13 +14,6 @@ app.use(bodyParser.json())
 // routes
 require('./routes')(app)
 
-// handle production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/public')))
-    // handle SPAs
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
-  }
-
 app.listen(port, () => console.log(`App is listening on port ${port}`))
 
 module.exports = app
