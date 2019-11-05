c091ccf6613b136e0d41b3a247f5f34d4118d8af
Danny <windate3411@gmail.com>
2019-11-05 15:37:01 +0800

    modify cron job (add time range)

--- server/config/cron.js
+++ server/config/cron.js
@@ -4,49 +4,59 @@ const Order = db.Order
 const Meal = db.Meal
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
-
+const moment = require('moment')
 //每日訂單更新(明日->今日) 以及庫存數量回復本周的設定值
-cron.schedule('00 00 * * *', async () => {
+cron.schedule('59 23 * * *', async () => {
+  try {
+    // 撈出所有今日的訂單
+    let orders = await Order.findAll({
+      include: [{ model: Meal, as: 'meals' }],
+      where: {
+        order_status: { [Op.like]: '今日' },
+        require_date: {
+          [Op.gte]: moment().subtract(1, 'day').toDate(),
+          [Op.lte]: moment().toDate()
+        }
+      }
+    })
 
-  // 撈出所有今日的訂單
-  let orders = await Order.findAll({
-    include: [{ model: Meal, as: 'meals' }],
-    where: {
-      order_status: { [Op.like]: '今日' }
-    }
-  })
+    // 加總今日訂單個別餐點的點餐量，以物件方式儲存供等一下的資料庫更新使用
+    let results = orders.reduce((obj, item) => {
+      if (item.meals[0].dataValues.id in obj) {
+        obj[item.meals[0].dataValues.id] += Number(item.amount)
+      } else {
+        obj[item.meals[0].dataValues.id] = Number(item.amount)
+      }
+      return obj
+    }, {})
 
-  // 加總今日訂單個別餐點的點餐量，以物件方式儲存供等一下的資料庫更新使用
-  let results = orders.reduce((obj, item) => {
-    if (item.meals[0].dataValues.id in obj) {
-      obj[item.meals[0].dataValues.id] += Number(item.amount)
-    } else {
-      obj[item.meals[0].dataValues.id] = Number(item.amount)
+    // 將餐點的數量藉由前一步的加總結果回復原始的數值(剩餘數量+今天點餐數量)
+    async function handleResult(results) {
+      for (item in results) {
+        const meal = await Meal.findByPk(item)
+        let currentAmount = meal.quantity
+        meal.update({ quantity: Number(currentAmount) + Number(results[item]) })
+      }
     }
-    return obj
-  }, {})
 
-  // 將餐點的數量藉由前一步的加總結果回復原始的數值(剩餘數量+今天點餐數量)
-  async function handleResult(results) {
-    for (item in results) {
-      const meal = await Meal.findByPk(item)
-      let currentAmount = meal.quantity
-      meal.update({ quantity: Number(currentAmount) + Number(results[item]) })
-    }
+    // 執行函數
+    handleResult(results)
+
+    // 撈出所有訂單狀態為明日的訂單並更新訂單狀態為今日
+    let orders_tmr = await Order.findAll({
+      where: {
+        order_status: { [Op.like]: '明日' }
+      }
+    })
+    orders_tmr.map(async item => {
+      await item.update({ order_status: '今日' })
+    })
+
+  } catch (error) {
+    console.log(error);
   }
 
-  // 執行函數
-  handleResult(results)
 
-  // 撈出所有訂單狀態為明日的訂單並更新訂單狀態為今日
-  let orders_tmr = await Order.findAll({
-    where: {
-      order_status: { [Op.like]: '明日' }
-    }
-  })
-  orders_tmr.map(async item => {
-    await item.update({ order_status: '今日' })
-  })
 })
 
 //每周菜單更新(下周->本周)
@@ -64,7 +74,6 @@ cron.schedule('00 00 * * 0', async () => {
   meals.map(async item => {
     await item.update({
       isServing: true,
-      nextServing: true,
       quantity: item.nextServing_quantity
     })
   })
