f294e86219c19d7c696bbe692341da50a46ad12f
Mike <mike.huang.mikank@gmail.com>
2019-11-08 16:47:17 +0800

    Update comment rating and image, and perfect stying for RWD

--- client/src/components/CommentMedia.vue
+++ client/src/components/CommentMedia.vue
@@ -6,29 +6,48 @@
       alt="user avatar"
     >
     <div class="media-body ">
-      <h5 class="m-0 media-title d-flex justify-content-between">
-        <span>{{ comment.User.name }}</span>
-        <span class="time">{{ comment.createdAt | timeTransform }}</span>
-      </h5>
-      <p class="media-rating mb-2">
-        <span class="rating">&#9733; {{ comment.rating }}</span>
-      </p>
-      <p class="media-description mb-4">
+      <div class="m-0 media-title">
+        <div class="media-title-left">
+          <span class="mr-2">{{ comment.User.name }}</span>
+          <RatingStars
+            :rating="comment.rating"
+            class="media-rating d-inline"
+          />
+        </div>
+        <span class="media-title-right mb-2 mb-sm-0">{{ comment.createdAt | timeTransform }}</span>
+      </div>
+      <p class="media-description mb-0 mt-1">
         {{ comment.user_text }}
       </p>
+      <img
+        src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+        class="file-image file-image-small my-3"
+        :class="{large: isZoomIn}"
+        alt="評論照片"
+        @click="isZoomIn = !isZoomIn"
+      >
     </div>
   </div>
 </template>
 
 <script>
+import RatingStars from '../components/RatingStars'
 import { timeTransformFilter } from '../utils/mixins'
 
 export default {
+  components: {
+    RatingStars
+  },
   mixins: [timeTransformFilter],
   props: {
     comment: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      isZoomIn: false
     }
   }
 }
@@ -48,12 +67,19 @@ export default {
     }
 
     &-title {
+        display: flex;
+        flex-direction: column;
         color: darken(color(secondary), 15%);
         font-size: size(sm);
 
-        .time {
+        &-right {
             font-size: size(xs);
             color: lighten(color(secondary), 40%);
+        }
+
+        @include response(sm) {
+            flex-direction: row;
+            justify-content: space-between;
         }
     }
 
@@ -64,6 +90,28 @@ export default {
     &-rating {
         color: color(primary);
         font-size: size(sm);
+    }
+}
+
+.file {
+    &-image {
+        object-fit: cover;
+        border-radius: .3rem;
+        width: 80px;
+        height: 60px;
+        cursor: zoom-in;
+        transition: all .2s ease-in;
+
+        &.large {
+            width: 200px;
+            height: 150px;
+            cursor: zoom-out;
+
+            @include response(md) {
+                width: 240px;
+                height: 180px;
+            }
+        }
     }
 }
 </style>
