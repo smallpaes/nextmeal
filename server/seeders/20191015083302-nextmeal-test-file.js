d1b301e851aa4e5bcf3ab60c696ccb21b6d3e558
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-03-13 22:13:34 +0000

    Update seeder id

--- server/seeders/20191015083302-nextmeal-test-file.js
+++ server/seeders/20191015083302-nextmeal-test-file.js
@@ -66,6 +66,7 @@ function createRest(store) {
   let restData = []
   for (let i = 0; i < store.length; i++) {
     const seedRest = {
+      id: id + 1,
       name: store[i].name.length > 30 ? store[i].name.slice(30) : store[i].name,
       description: store[i].description.substring(0, 300),
       tel: `02-${randomPhone(4)}-${randomPhone(4)}`,
@@ -93,6 +94,7 @@ function creatMeal(stores) {
   for (let i = 0; i < stores.length; i++) {
     let random = Math.floor(Math.random() * foodImg.length)
     const seedMeal = {
+      id: id + 1,
       name: foodImg[random].name.length > 30 ? foodImg[random].name.slice(30) : foodImg[random].name,
       image: foodImg[random].image,
       RestaurantId: i + 1,
@@ -189,6 +191,7 @@ function orderThing(start, end) {
     }
     orderPast = new Date(moment(past).subtract(1, 'days'))
     const seedOrders = {
+      id: id + 1,
       UserId: userId,
       require_date: past,
       order_date: orderPast,
@@ -199,6 +202,7 @@ function orderThing(start, end) {
       updatedAt: past
     }
     const seedOrderItem = {
+      id: id + 1,
       OrderId: i + 1,
       MealId: orderMeal,
       quantity: random,
@@ -207,6 +211,7 @@ function orderThing(start, end) {
     }
     if (i < 158) {
       const seedComment = {
+        id: id + 1,
         UserId: userId,
         user_text: user_text,
         rating: Math.floor(Math.random() * 5 + 1),
@@ -257,6 +262,7 @@ module.exports = {
     return queryInterface.bulkInsert("Subscriptions",
       Array.from({ length: users.length - stores.length - 1 }).map((item, index) => (
         {
+          id: index + 1,
           UserId: index === 0 ? index + 2 : index + stores.length + 2,
           sub_name: index > 90 ? '輕量型' : '滿足型',
           sub_price: index > 90 ? 1000 : 2000,
