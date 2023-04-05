b83619c027cab6f00ca8e8cf70954f7e1731526a
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-04-05 23:24:42 +0100

    Implement ImageKit in DropdownBanner component

--- client/src/utils/image-url.js
+++ client/src/utils/image-url.js
@@ -1,2 +1,3 @@
 export const CARD_PLACEHOLDER_FULL_URL = 'https://ik.imagekit.io/mikank/Nextmeal/tr:n-card/Placeholder/plain.png'
 export const CARD_PLACEHOLDER_RELATIVE_URL = '/tr:n-card/Placeholder/plain.png'
+export const CARD_PLACEHOLDER_ORIGINAL_RELATIVE_URL = '/Placeholder/plain.png'
